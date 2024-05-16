479876823ab18fe82301b18345e49fa697d0c3fd
Guilherme Peres <guilherme.aperes17@gmail.com>
2024-05-16 17:03:03 -0300

    feat: create component "pensamento"

---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css'
+})
+export class PensamentoComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  @Input() pensamento = {
+    conteudo: "",
+    autoria: "",
+    modelo: ""
+  }
+
+  larguraPensamento():string {
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }else{
+      return 'pensamento-p'
+    }
+  }
+}
